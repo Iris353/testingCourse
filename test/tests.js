240915a2e690785813679e6647f35d9bbae5c289
Iris353 <35698747+Iris353@users.noreply.github.com>
2020-11-25 23:20:03 +0200

    added test and file to be tested for test

---
+++ test/tests.js
@@ -0,0 +1,13 @@
+/* test/sum.js */
+
+var sum = require('../codes/sum.js');
+var expect = require('chai').expect;
+
+describe('dummytest', function() {
+
+  context('without arguments', function() {
+    it('should return 0', function() {
+      expect(0).to.equal(0)
+    })
+  })
+})
