28bf6d5d33658b255a8124d53eaf7663410d031f
elkahu <elli.hurme@hotmail.com>
2020-11-29 17:34:19 +0200

    add coveralls

--- test/tests.js
+++ test/tests.js
@@ -1,14 +1,18 @@
 /* test/sum.js */
 
-import sum from '../codes/sum.js';
-import pkg from 'chai';
+import sum from "../codes/sum.js";
+import pkg from "chai";
 const { expect } = pkg;
 
-describe('dummytest', function() {
+describe("dummytest", function() {
 
-  context('without arguments', function() {
-    it('should return 0', function() {
+  context("without arguments", function() {
+    it("should return 0", function() {
       expect(0).to.equal(0);
+    })
+
+    it("should be two", () =>{
+      expect(sum(1,1)).to.equal(2);
     })
   })
 })
