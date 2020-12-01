6a4c399735ec3b7497c83975c37436b1ff596f74
elkahu <elli.hurme@hotmail.com>
2020-12-01 17:16:06 +0200

    equals-> equal

--- test/chunk.test.js
+++ test/chunk.test.js
@@ -10,22 +10,22 @@ describe("Test chunk function", function() {
     }) 
 
     it("should return correct chuncks when mod == 0", function() {
-        assert.equals(chunk(['a', 'b', 'c', 'd'], 2), [['a', 'b'], ['c', 'd']]);
+        assert.equal(chunk(['a', 'b', 'c', 'd'], 2), [['a', 'b'], ['c', 'd']]);
     })
     
     it("should return correct chuncks when no size", function() {
-        assert.equals(chunk(['a', 'b', 'c', 'd']), [['a'], ['b'], ['c'], ['d']]);
+        assert.equal(chunk(['a', 'b', 'c', 'd']), [['a'], ['b'], ['c'], ['d']]);
     })
 
     it("should return correct chunck when size is same as lenght of array", function() {
-        assert.equals(chunk(['a', 'b', 'c', 'd'], 4), [['a', 'b', 'c', 'd']]);
+        assert.equal(chunk(['a', 'b', 'c', 'd'], 4), [['a', 'b', 'c', 'd']]);
     })
 
     it("should return correct chuncks when mod /= 0", function() {
-        assert.equals(chunk(['a', 'b', 'c', 'd'], 3), [['a', 'b', 'c'], ['d']]);
+        assert.equal(chunk(['a', 'b', 'c', 'd'], 3), [['a', 'b', 'c'], ['d']]);
     }) 
 
     it("should return empty array when size is zero", function() {
-        assert.equals(chunk(['a', 'b', 'c', 'd'], 0), []);
+        assert.equal(chunk(['a', 'b', 'c', 'd'], 0), []);
     }) 
 })
--- test/clamp.test.js
+++ test/clamp.test.js
@@ -11,12 +11,12 @@ describe("Test clamp function", function() {
     }) 
 
     it("should clamp outbound number to bounds", function() {
-        assert.equals(clamp(-10,-5,5), -5);
-        assert.equals(clamp(10,-5,15), 10);
+        assert.equal(clamp(-10,-5,5), -5);
+        assert.equal(clamp(10,-5,15), 10);
     })
 
     it("should clamp in obound number to bounds", function() {
-        assert.equals(clamp(-10,-5,-7), -7);
-        assert.equals(clamp(10,-5,5), 5);
+        assert.equal(clamp(-10,-5,-7), -7);
+        assert.equal(clamp(10,-5,5), 5);
     })
 })
