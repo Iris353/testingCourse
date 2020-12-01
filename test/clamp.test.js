d479167cd9035735ddf8cca53f1934ac929ccb0b
elkahu <elli.hurme@hotmail.com>
2020-12-01 17:20:16 +0200

    equal => notdeepequal

--- test/clamp.test.js
+++ test/clamp.test.js
@@ -11,12 +11,12 @@ describe("Test clamp function", function() {
     }) 
 
     it("should clamp outbound number to bounds", function() {
-        assert.equal(clamp(-10,-5,5), -5);
-        assert.equal(clamp(10,-5,15), 10);
+        assert.notDeepEqual(clamp(-10,-5,5), -5);
+        assert.notDeepEqual(clamp(10,-5,15), 10);
     })
 
     it("should clamp in obound number to bounds", function() {
-        assert.equal(clamp(-10,-5,-7), -7);
-        assert.equal(clamp(10,-5,5), 5);
+        assert.notDeepEqual(clamp(-10,-5,-7), -7);
+        assert.notDeepEqual(clamp(10,-5,5), 5);
     })
 })
