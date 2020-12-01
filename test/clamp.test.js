ead1b89d4880ea2f30702a96d085de5939792b71
elkahu <elli.hurme@hotmail.com>
2020-12-01 17:13:32 +0200

    add clamp tests

---
+++ test/clamp.test.js
@@ -0,0 +1,22 @@
+import clamp from "../codes/clamp.js";
+import pkg from "chai";
+const { expect, assert } = pkg;
+
+describe("Test clamp function", function() {
+
+    it("should throw error when invalid params", function() {
+        assert.throw(() => { clamp(object, object, object) }, Error);
+        assert.throw(() => { clamp("1", "2","3") }, Error);
+        assert.throw(() => { clamp(null, null,null) }, Error);
+    }) 
+
+    it("should clamp outbound number to bounds", function() {
+        assert.equals(clamp(-10,-5,5), -5);
+        assert.equals(clamp(10,-5,15), 10);
+    })
+
+    it("should clamp in obound number to bounds", function() {
+        assert.equals(clamp(-10,-5,-7), -7);
+        assert.equals(clamp(10,-5,5), 5);
+    })
+})
