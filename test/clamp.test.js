84e0327aa31f29a09492bd36148062f9ca777f20
elkahu <elli.hurme@hotmail.com>
2020-12-01 17:26:25 +0200

    fix clamp

--- test/clamp.test.js
+++ test/clamp.test.js
@@ -11,12 +11,12 @@ describe("Test clamp function", function() {
     }) 
 
     it("should clamp outbound number to bounds", function() {
-        assert.notDeepEqual(clamp(-10,-5,5), -5);
-        assert.notDeepEqual(clamp(10,-5,15), 10);
+        expect(clamp(-10,-5,5)).to.eql(-5);
+        expect(clamp(10,-5,15).to.eql(10);
     })
 
     it("should clamp in obound number to bounds", function() {
-        assert.notDeepEqual(clamp(-10,-5,-7), -7);
-        assert.notDeepEqual(clamp(10,-5,5), 5);
+        expect(clamp(-10,-5,-7)).to.eql(-7);
+        expect(clamp(10,-5,5)).to.eql(5);
     })
 })
