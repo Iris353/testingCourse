43f8b78dc6a72440d286263170e5fbe68f88690c
Iris353 <35698747+Iris353@users.noreply.github.com>
2020-12-06 21:28:36 +0200

    test fixes

--- test/castArray.test.js
+++ test/castArray.test.js
@@ -5,11 +5,11 @@ const { expect, assert } = pkg;
 describe("Test castArrayfunction", function() {
 
     it("should return an array", function() {
-        expect(castArray(1).to.eql([1]));
-        expect(castArray({ 'a': 1 }).to.eql({ 'a': 1 }));
-        expect(castArray('abc').to.eql(['abc']));
-        expect(castArray(null).to.eql(null));
-        expect(castArray(undefined).to.eql(undefined));
-        expect(castArray().to.eql([]));
+        expect(castArray(1)).to.eql([1]);
+        expect(castArray({ 'a': 1 })).to.eql([{ 'a': 1 }]);
+        expect(castArray('abc')).to.eql(['abc']);
+        expect(castArray(null)).to.eql([null]);
+        expect(castArray(undefined)).to.eql([undefined]);
+        expect(castArray()).to.eql([]);
     })
 })
--- test/upperFirst.test.js
+++ test/upperFirst.test.js
@@ -5,8 +5,8 @@ const { expect, assert } = pkg;
 describe("Test upperFirst function", function() {
 
     it("should return string with first letter capitalized", function() {
-        expect(upperFirst('ostoslista').to.eql('Ostoslista'));
-        expect(upperFirst('OSTOSLISTA').to.eql('OSTOSLISTA'));
-        expect(upperFirst('Ostoslista').to.eql('Ostoslista'));
+        expect(upperFirst('ostoslista')).to.eql('Ostoslista');
+        expect(upperFirst('OSTOSLISTA')).to.eql('OSTOSLISTA');
+        expect(upperFirst('Ostoslista')).to.eql('Ostoslista');
     })
 })
