94800754a2a17ae843dbed29752d5c1ac776ed3b
Iris353 <35698747+Iris353@users.noreply.github.com>
2020-12-06 16:57:36 +0200

    castarray and upperfirst added

---
+++ test/castArray.test.js
@@ -0,0 +1,15 @@
+import castArray from "../codes/castArray.js";
+import pkg from "chai";
+const { expect, assert } = pkg;
+
+describe("Test castArrayfunction", function() {
+
+    it("should return an array", function() {
+        expect(castArray(1).to.eql([1]));
+        expect(castArray({ 'a': 1 }).to.eql({ 'a': 1 }));
+        expect(castArray('abc').to.eql(['abc']));
+        expect(castArray(null).to.eql(null));
+        expect(castArray(undefined).to.eql(undefined));
+        expect(castArray().to.eql([]));
+    })
+})
---
+++ test/upperFirst.test.js
@@ -0,0 +1,12 @@
+import upperFirst from "../codes/upperFirst.js";
+import pkg from "chai";
+const { expect, assert } = pkg;
+
+describe("Test upperFirst function", function() {
+
+    it("should return string with first letter capitalized", function() {
+        expect(upperFirst('ostoslista').to.eql('Ostoslista'));
+        expect(upperFirst('OSTOSLISTA').to.eql('OSTOSLISTA'));
+        expect(upperFirst('Ostoslista').to.eql('Ostoslista'));
+    })
+})
